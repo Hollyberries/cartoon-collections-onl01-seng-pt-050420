aae76fc256eb9694000a49a6ab8108cc3225e3f2
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-14 03:17:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect {|snack| snack(cheese_types).include?}
+  array.detect {|snack| cheese_types.include?(snack)}
   
  end
